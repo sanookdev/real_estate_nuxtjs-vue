6c93bff7809807ac7a9fcd4bd3753d34d1faca8f
sanookdev <supaporn.dev@gmail.com>
2026-01-01 09:18:29 +0700

    updated config db

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,7 +5,8 @@ const pool = mysql.createPool({
     host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_NAME || 'asset_sale_db',
+    database: process.env.DB_NAME || 'test',
+    port: process.env.DB_PORT || 3306,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
